d3a4bcb78fc3638b74dc1bed49515995939eeb40
Aleksei Flis <fliseno1k@gmail.com>
2021-05-17 03:43:02 +1000

    render svg path by new props

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,40 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-export default function Canvas({ layers }) {
-    
+export default function Canvas({ computedPath, gradientColors }) {
     return (
-        <div className="flex flex-auto mr-4 rounded-lg overflow-hidden shadow-sm bg-white">
-            <svg
-                width="100%"
-                height="100%"
-                viewBox={`0 0 1440 500`}
-                xmlns="http://www.w3.org/2000/svg"
-                className="f-full transition duration-300 ease-in-out delay-150"
-                preserveAspectRatio="none"
-            >
-                {
-                    layers.map((layer, lIndex) => (
-                        <g key={layer.id}>
-                            <defs>
-                                <linearGradient id={'gradient' + lIndex}>
-                                    {
-                                        layer.colors.map((color, i) => (
-                                            <stop key={i} offset={color.offset + '%'} stopColor={`rgba(${color.color.join(', ')}, ${(lIndex + 1) / layers.length})`} />
-                                        ))
-                                    }
-                                </linearGradient>
-                            </defs>
-                            <path
-                                d={layer.path.d}
-                                stroke={layer.path.strokeColor}
-                                strokeWidth={layer.path.strokeWidth}
-                                fill={`url(#${'gradient' + lIndex})`}
-                                className="transition-all duration-300 ease-in-out delay-150"
-                            />
-                        </g>
-                    ))
-                }
-            </svg>
+        <div className="relative flex flex-auto mr-4 rounded-lg overflow-hidden shadow-sm bg-white">
+            <div className="absolute bottom-0 left-0 w-full">
+                <svg
+                    width="100%"
+                    height="100%"
+                    viewBox={`0 0 1440 ${computedPath.svg.height}`}
+                    xmlns={computedPath.svg.xmlns}
+                    className="f-full transition duration-500 ease-in-out delay-150"
+                    // preserveAspectRatio="none"
+                >
+                    {
+                        computedPath.svg.path.map((path, i) => (
+                            <g key={i}>
+                                <defs>
+                                    <linearGradient id={'gradient' + i}>
+                                        <stop 
+                                            offset="5%" 
+                                            stopColor={gradientColors.first} 
+                                            stopOpacity={(i + 1) / computedPath.svg.path.length}
+                                        />
+                                        <stop 
+                                            offset="95%" 
+                                            stopColor={gradientColors.last} 
+                                            stopOpacity={(i + 1) / computedPath.svg.path.length}
+                                        />
+                                    </linearGradient>
+                                </defs>
+                                <path
+                                    d={path.d}
+                                    stroke={path.strokeColor}
+                                    strokeWidth={path.strokeWidth}
+                                    fill={`url(#${'gradient' + i})`}
+                                    className="transition-all duration-500 ease-in-out delay-150"
+                                />
+                            </g>
+                        ))
+                    }
+                </svg>
+            </div>
         </div>
     );
 };
