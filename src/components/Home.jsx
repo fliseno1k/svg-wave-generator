43db0d9a8fdbedde38d2517b6a5d2badf68b51cf
Aleksei Flis <fliseno1k@gmail.com>
2021-05-20 01:25:28 +1000

    Wrap by modal provider

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
-import Header from './Header';
+import { ModalProvider } from '../contexts/ModalContext';
 
+import Header from './Header';
 import Generator from './Generator';
+import DownloadModal from './DownloadModal';
 
 export default function Home() { 
     return (
-        <div className="relative min-h-screen h-screen">
-            <Header />
-            <main className="w-full">
-                <div className="bg-gray-100 mx-auto px-2 pt-12 text-center md:pt-24 md:pb-16">
-                    <h1 className="text-5xl font-bold text-gray-400">Make some waves!</h1>
-                </div>
-                <Generator />
-            </main>
+        <div className="relative min-h-screen">
+            <ModalProvider>
+                <Header />
+                <main className="w-full">
+                    <Generator />
+                </main>
+                <DownloadModal />
+            </ModalProvider>
         </div>
     );
 }
